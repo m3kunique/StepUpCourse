f6a722c5debaad1c9695ef7ce820d86e07c566fa
m3k01 <tmk2408@gmail.com>
2024-07-05 14:44:43 +0300

    feat OOP : solved Student

--- OOP/Start.java
+++ OOP/Start.java
@@ -1,7 +1,11 @@
 package OOP;
 
+import java.util.ArrayList;
+
 public class Start {
     public static void main(String[] args) {
-
+        var student = new Student("Mikhail");
+        System.out.println(student.getMarks());
+        System.out.println(student.toString());
     }
 }
