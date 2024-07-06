72fff3881cfc79051a6eda92665c97a6c41ac75d
m3k01 <tmk2408@gmail.com>
2024-07-06 18:04:51 +0300

    feat OOP : solved strLength

--- OOP/LengthImpl.java
+++ OOP/LengthImpl.java
@@ -7,4 +7,7 @@ public class LengthImpl{
         for (Lengthable len : ob) sum+= len.length();
         return sum;
     }
+    public double getLength(String str, Lengthable... ob) {
+        return this.getLength(ob)+str.length();
+    }
 }
