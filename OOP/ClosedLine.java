2537792090c01d805949281d7cdc75b36516f70b
m3k01 <tmk2408@gmail.com>
2024-07-06 17:37:07 +0300

    fix OOP : fixed ClosedLine

--- OOP/ClosedLine.java
+++ OOP/ClosedLine.java
@@ -7,10 +7,6 @@ public class ClosedLine extends PolyLine{
     }
 
     public double length() {
-        double sum = super.length(), len1, len2;
-        len1 = points[0].getX() - points[points.length-1].getX();
-        len2 = points[0].getY() - points[points.length-1].getY();
-        sum += Math.sqrt(len1 * len1 + len2 * len2);
-        return sum;
+        return super.length() + new Line(points[0], points[points.length-1]).length();
     }
 }
