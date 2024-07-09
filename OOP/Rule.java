4d33a4414f64f6be4def138ead05470630b90ba9
m3k01 <tmk2408@gmail.com>
2024-07-09 21:29:40 +0300

    feat OOP : solved StudentRule

---
+++ OOP/Rule.java
@@ -0,0 +1,5 @@
+package OOP;
+
+public interface Rule {
+    boolean check(int x);
+}
--- OOP/Start.java
+++ OOP/Start.java
@@ -1,22 +1,11 @@
 package OOP;
 
-import javax.crypto.spec.PSource;
-
 public class Start {
     public static void main(String[] args) {
-        var square = new Square(10, new Point(1, 1));
-        var circle = new Circle(new Point(1,1), 10);
-        var rectengle = new Rectangle(10, 10, new Point(1, 1));
-        System.out.println(square.square());
-        System.out.println(circle.square());
-        System.out.println(rectengle.square());
-    }
-
-    public static double areaForAllFigures(Shapes... shapes) {
-        double sum=0;
-        for (Shapes shape : shapes) {
-            sum+=shape.square();
-        }
-        return sum;
+        var stud = new Student("Mikhaiil", x->x%2==0);
+        stud.addMark(4);
+        stud.addMark(5);
+        stud.addMark(6);
+        System.out.println(stud.toString());
     }
 }
