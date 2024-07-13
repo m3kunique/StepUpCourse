b97884b2ab55603caea6d5356386b7810deea454
m3k01 <tmk2408@gmail.com>
2024-07-14 00:43:02 +0300

    feat OOP : solved OperationAttemptException

---
+++ OOP/OperationAttemptException.java
@@ -0,0 +1,19 @@
+package OOP;
+
+public class OperationAttemptException extends Exception{
+    public OperationAttemptException() {
+        super();
+    }
+
+    public OperationAttemptException(String message) {
+        super(message);
+    }
+
+    public OperationAttemptException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public OperationAttemptException(Throwable cause) {
+        super(cause);
+    }
+}
