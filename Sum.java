8a1f4dd9b5373528cbf6504a7512b0c1274c4354
m3k01 <tmk2408@gmail.com>
2024-07-14 00:34:28 +0300

    fix OOP : fixed Sum problem

--- Sum.java
+++ Sum.java
@@ -2,9 +2,8 @@ public class Sum {
     public static void main(String[] args) {
         double sum=0;
         for(String s:args){
-            try{
+            if (s.matches("-?\\d+(\\.\\d+)?"))
                 sum+=Double.parseDouble(s);
-            }catch(NumberFormatException _){}
         }
         System.out.println(sum);
     }
